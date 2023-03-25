53d91b4a53d2d5cc1c893dfc70fa7431e6d3f2a1
Olafi-MooN <alefmastertutor@gmail.com>
2023-03-25 09:30:57 -0300

    updates

--- src/components/section-pages/projects-block/index.tsx
+++ src/components/section-pages/projects-block/index.tsx
@@ -1,67 +1,48 @@
 import Atropos from 'atropos/react';
-import "./Projects.css"
+import './Projects.css';
 
 import comuBibliPNG from '../../../public/images/Comubibli.png';
 import { useModalContext } from '../../../Context/ModalContext';
+import { useEffect, useState } from 'react';
+import { firebaseApp } from '../../../firebase';
+import { collection, doc, getDoc, getFirestore } from 'firebase/firestore';
 
 const ProjectsBlock = () => {
-  const { openModal } = useModalContext();
+	const { openModal } = useModalContext();
+	const [list, setList] = useState<any[]>([]);
 
-  return (
-    <section className="container-projects-block" id="projects">
-      <div className="content-benefits">
-        <h3 className="benefits-aba">Projetos</h3>
-        <h1 className="effect-phrase">Uma pequena seleção dos meus projetos</h1>
-      </div>
-      <div className="content-projects-grid">
-        <div className="projects-block" onClick={() => openModal({title: "Green Chain - Landing Page", childNode: <></>})}>
-          <Atropos
-            highlight={true}
-            shadow={true}
-            rotateTouch={window.innerWidth > 1000}
-            rotate={window.innerWidth > 1000}
-            className="projects-block-atr1"
-          >
-            <img src="https://github.com/Olafi-MooN/StartSe/raw/main/src/image/page-initial.png" alt="projeto 1" height="100%" width="100%"/>
-          </Atropos>
-        </div>
-        <div className="projects-block" onClick={() => openModal({title: "Be The Hero", childNode: <></>})}>
-          <Atropos
-            highlight={true}
-            shadow={true}
-            rotateTouch={window.innerWidth > 1000}
-            rotate={window.innerWidth > 1000}
-            className="projects-block-atr2"
-          >
-            <img src="https://i.ytimg.com/vi/ZYzQSgAuYRM/maxresdefault.jpg" alt="projeto 2" height="100%" width="100%"/>
-          </Atropos>
-        </div>
-        <div className="projects-block" onClick={() => openModal({title: "Tela de Login", childNode: <></>})}>
-          <Atropos
-            highlight={true}
-            shadow={true}
-            rotateTouch={window.innerWidth > 1000}
-            rotate={window.innerWidth > 1000}
-            className="projects-block-atr3"
-          >
-            <img src="https://myportfolioalef.herokuapp.com/img/telaLogin.png" alt="projeto 1" height="100%" width="100%"/>
-          </Atropos>
-        </div>
-        <div className="projects-block" onClick={() => openModal({title: "ComuBibli - Biblioteca Comunitária", childNode: <></>})}>
-          <Atropos
-            highlight={true}
-            shadow={true}
-            rotateTouch={window.innerWidth > 1000}
-            rotate={window.innerWidth > 1000}
-            className="projects-block-atr4"
-          >
-            <img src={comuBibliPNG} alt="projeto 1" height="100%" width="100%"/>
-          </Atropos>
-        </div>
-       
-      </div>
-    </section>
-  );
-}
+	useEffect(() => {
+		const db = getFirestore(firebaseApp);
+		const docRef = doc(db, 'projects', 'AJKFtOr7VwiJsIUQKjCr');
+		getDoc(docRef).then((docSnap) => {
+			if (docSnap.exists()) {
+				setList(docSnap.data().list);
+				console.log('Document data:', docSnap.data());
+			} else {
+				console.log('No such document!');
+			}
+		});
+	}, []);
+
+	return (
+		<section className="container-projects-block" id="projects">
+			<div className="content-benefits">
+				<h3 className="benefits-aba">Projetos</h3>
+				<h1 className="effect-phrase">Uma pequena seleção dos meus projetos</h1>
+			</div>
+			<div className="content-projects-grid">
+				{list.map((x) => {
+					return (
+						<div className="projects-block" onClick={() => openModal({ title: x.title, childNode: <iframe src={x?.site} height={'100%'} width={'100%'}></iframe> })}>
+							<Atropos highlight={true} shadow={true} rotateTouch={window.innerWidth > 1000} rotate={window.innerWidth > 1000} className="projects-block-atr1">
+								<img src={x.thumbnail} alt={x?.title} height="100%" width="100%" />
+							</Atropos>
+						</div>
+					);
+				})}
+			</div>
+		</section>
+	);
+};
 
 export { ProjectsBlock };
--- src/pages/landing-page/index.tsx
+++ src/pages/landing-page/index.tsx
@@ -1,40 +1,38 @@
 // libraries
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 
 // Components
-import ScrollTrigger from "gsap/ScrollTrigger";
-import { HeaderMenu } from "../../components/header-menu";
-import { ProfileBlock } from "../../components/section-pages/profile-block";
-import { scrollEffects, gsap } from "../../components/shared/utils/animate-gsap";
-import { BenefitsBlock } from "../../components/section-pages/benefits-block";
-import { ProjectsBlock } from "../../components/section-pages/projects-block";
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import { HeaderMenu } from '../../components/header-menu';
+import { ProfileBlock } from '../../components/section-pages/profile-block';
+import { scrollEffects, gsap } from '../../components/shared/utils/animate-gsap';
+import { BenefitsBlock } from '../../components/section-pages/benefits-block';
+import { ProjectsBlock } from '../../components/section-pages/projects-block';
 
 // Styles
-import "./landing-page.css"; 
-import { TextMotivation } from "../../components/text-motivation";
-import { Modal } from "../../components/Modal";
+import './landing-page.css';
+import { TextMotivation } from '../../components/text-motivation';
+import { Modal } from '../../components/Modal';
 
 gsap.registerPlugin(ScrollTrigger);
 
 function LandingPage() {
+	useEffect(() => (window.innerWidth > 1000 ? scrollEffects() : () => {}), []);
 
-
-  useEffect(() => window.innerWidth > 1000 ? scrollEffects() : () => { }, [])
-  
-  return (
-    <>
-      <div className="container-landing-page">
-        <HeaderMenu></HeaderMenu>
-        <div className="scroll-gsap-profile">
-          <ProfileBlock></ProfileBlock>
-        </div>
-        <TextMotivation text={"Transforme a <br> sua ideia em <br/> um negócio digital."}></TextMotivation>
-        <BenefitsBlock></BenefitsBlock>
-        <ProjectsBlock></ProjectsBlock>
-      </div>
-      <Modal/>
-    </>
-  )
+	return (
+		<>
+			<div className="container-landing-page">
+				<HeaderMenu></HeaderMenu>
+				<div className="scroll-gsap-profile">
+					<ProfileBlock></ProfileBlock>
+				</div>
+				<TextMotivation text={'Transforme a <br> sua ideia em <br/> um negócio digital.'}></TextMotivation>
+				<BenefitsBlock />
+				<ProjectsBlock />
+			</div>
+			<Modal />
+		</>
+	);
 }
 
 export { LandingPage };
